e8d28d7f617883ef3f01b70c3acbad2e23515201
celomassao <celomassao@gmail.com>
2017-03-19 21:24:17 -0300

    formulario da promocao

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Http } from '@angular/http';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -8,8 +8,16 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  nome;
+  email;
+  constructor(public navCtrl: NavController, private http:Http) {
 
   }
 
+  cadastrar(){
+    this.http.get('http://api.codeam.com.br/busam/promocao.php?nome=' + this.nome + '&email=' + this.email)
+      .subscribe(data => {
+        alert(this.nome + ", parabéns, você já está concorrendo!");
+      });
+  }
 }
