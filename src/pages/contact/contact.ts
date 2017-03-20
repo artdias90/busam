19375ccf5d2929930c30d1026301f45c5a99ed12
celomassao <celomassao@gmail.com>
2017-03-19 22:53:39 -0300

    formulario pronto

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController  } from 'ionic-angular';
+import { HomePage } from "../home/home";
 
 @Component({
   selector: 'page-contact',
@@ -10,14 +11,37 @@ export class ContactPage {
 
   nome;
   email;
-  constructor(public navCtrl: NavController, private http:Http) {
+  constructor(public navCtrl: NavController, private http:Http, public alertCtrl: AlertController) {
 
   }
 
-  cadastrar(){
+  cadastrar() {
     this.http.get('http://api.codeam.com.br/busam/promocao.php?nome=' + this.nome + '&email=' + this.email)
       .subscribe(data => {
         alert(this.nome + ", parabéns, você já está concorrendo!");
+        this.navCtrl.push(HomePage, {});
       });
+  }
+
+  regulamento() {
+    let confirm = this.alertCtrl.create({
+      title: 'Regras da Promoção',
+      message: '1- O sorteio será de um ovo ao leite Kopenhagen de 150 gramas <br><br> 2- Para concorrer basta o participante cadastrar o nome e o e-mail. É totalmente grátis. <br><br> 3- O sorteio será realizado no dia 14/04. <br><br> 4- O ganhador será comunicado somente pelo e-mail cadastrado, não obtendo resposta, haverá um novo sorteio. <br><br> 5- O aplicativo não tem nenhuma relação com a Kopenhagen, apenas utilizou seu produto como prêmio.',
+      buttons: [
+        {
+          text: 'Discordo',
+          handler: () => {
+            this.navCtrl.push(HomePage, {});
+          }
+        },
+        {
+          text: 'Concordo',
+          handler: () => {
+            console.log('Concordou');
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 }
