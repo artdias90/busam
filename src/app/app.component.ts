19e7d8075e527787e02f5d220ec6b6c7ec7426fc
Celomassao <celomassao@gmail.com>
2017-05-11 09:50:52 -0300

    onesignal de novo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
+// $ ionic plugin add --save onesignal-cordova-plugin
+// $ npm install --save @ionic-native/onesignal
+
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StartPage } from '../pages/start/start';
+import { OneSignal } from '@ionic-native/onesignal';
 
 
 @Component({
@@ -9,9 +13,13 @@ import { StartPage } from '../pages/start/start';
 export class MyApp {
   rootPage: any = StartPage;
   
-  constructor(platform: Platform) {
+  constructor(platform: Platform, private oneSignal: OneSignal) {
     platform.ready().then(() => {
-
+			this.oneSignal.startInit('36133896-e5f4-45a8-8661-3e31bd6806f4', '863552885140');
+			this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+			this.oneSignal.handleNotificationReceived().subscribe(() => {});
+			this.oneSignal.handleNotificationOpened().subscribe(() => {});
+			this.oneSignal.endInit();
     });
   }
 }
